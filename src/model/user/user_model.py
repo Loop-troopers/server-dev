c037a844d69e85d667d3db99cae5e7546b4d258c
Sritone <sritone723@gmail.com>
2024-05-25 21:19:32 +0900

    feat: 사용자 모델 추가

---
+++ src/model/user/user_model.py
@@ -0,0 +1,30 @@
+import sqlite3
+import asyncio
+
+
+
+def create_user(username, password, email):
+    conn = sqlite3.connect("../db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO user (username, password, email) VALUES (?, ?, ?)",
+        (username, password, email)
+    )
+    conn.commit()
+    conn.close()
+
+def read_user_by_username(username):
+    conn = sqlite3.connect("../db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user WHERE username = ?", (username,))
+    user = cursor.fetchone()
+    conn.close()
+    return user
+
+def read_user_by_email(email):
+    conn = sqlite3.connect("../db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user WHERE email = ?", (email,))
+    user = cursor.fetchone()
+    conn.close()
+    return user
