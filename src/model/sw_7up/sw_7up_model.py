52bf1e2418233d3e3835527f4f26d40ef2f145c7
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-06-01 16:52:36 +0900

    remove: 공지사항 모델 통합에 따른 폴더 삭제

--- src/model/sw_7up/sw_7up_model.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-import asyncio
-from src.api.sw_7up.scrape import scrape_sw7up_notice
-from src import constants
-
-
--- src/model/sw_major/sw_major_model.py
+++
@@ -1,3 +0,0 @@
-import sqlite3
-from src.api.sw_major.scrape import scrape_sw_major_notice
-from src import constants
